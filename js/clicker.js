35a121b0679d68c7df91e2f059ddbf2267e9c886
Jonathan Sexton <joncsexton@gmail.com>
2018-08-20 06:29:08 -0500

    getting the grid established

--- js/clicker.js
+++ js/clicker.js
@@ -1,4 +1,4 @@
-let catImg = document.querySelector(".cat_img");
+let catImg = document.querySelector(".cat_img1");
 let clickCounter = document.querySelector(".numClicks");
 
 catImg.addEventListener("click", function() {
