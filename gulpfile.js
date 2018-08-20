43f5a49e7b670463afb8789c46e939ca80519e8c
Jonathan Sexton <joncsexton@gmail.com>
2018-08-20 05:05:03 -0500

    added normalize.css and cat image

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-const gulp = require("gulp");
-const browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
 
-gulp.task('browser-sync', function () {
-  browserSync.init({
-     server: {
-        baseDir: './',
-     }
-  });
+// Static server
+gulp.task("browser-sync", function() {
+browserSync.init({
+  server: {
+    baseDir: "./",
+  }
+});
+    gulp.watch(['./*.html' ,'js/*.js', 'css/*.css']).on('change', browserSync.reload);
 });
