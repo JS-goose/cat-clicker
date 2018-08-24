55b0eae70ac729fc14fd32c1ba29e57bdcb7ea10
Jonathan Sexton <joncsexton@gmail.com>
2018-08-24 16:01:17 -0500

    Updated cat names and declared globlas

--- js/clicker.js
+++ js/clicker.js
@@ -1,10 +1,19 @@
+// Global Variables
+const numClicks = document.querySelector("#numClicks");
+let catOne = document.querySelector(".gidget");
+let imgDisplay = document.querySelector(".imgDisplay");
+let catName = document.querySelector(".catName");
+
 // Constructor
 class Cat {
-  constructor (name, img) {
+  constructor(name, img) {
     this.name = name;
     this.img = img;
     this.clicks = 0;
   }
-}
 
-let gidget = new Cat('gidget', '../img/gidget.jpg');
+  addClicks() {
+    console.log("addClicks is working");
+    this.numClicks++;
+  }
+}
