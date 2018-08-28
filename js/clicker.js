3b0d8df900ce85921f5f7c06f0939be6acf7bfbb
Jonathan Sexton <joncsexton@gmail.com>
2018-08-28 05:10:08 -0500

    updated click events to reflect image path

--- js/clicker.js
+++ js/clicker.js
@@ -1,6 +1,7 @@
 // Global Variables
 const numClicks = document.querySelector("#numClicks");
 let catOne = document.querySelector(".gidget");
+let catTwo = document.querySelector(".bethany");
 let imgDisplay = document.querySelector(".imgDisplay");
 let catName = document.querySelector(".catName");
 
@@ -19,8 +20,11 @@ class Cat {
 }
 
 catOne.addEventListener('click', function() {
-  const gidget = new Cat('Gidget', '../img/gidget.jpg');
-  imgDisplay.src = '../img/gidget.jpg';
+  const gidget = new Cat('Gidget', './img/gidget.jpg');
+  imgDisplay.src = gidget.img;
 });
 
-
+catTwo.addEventListener('click', function() {
+  const bethany = new Cat('Bethany', './img/bethany.jpg')
+  imgDisplay.src = bethany.img;
+});
